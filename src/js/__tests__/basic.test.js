4769aef08462257d4ed58a4556adcdce04dbd668
Maden5126 <madenon123@gmail.com>
2021-09-19 13:52:55 +0300

    toBe check added

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -16,3 +16,19 @@ test('unitOrder', () => {
   ];
   expect(received).toEqual(expected);
 });
+test('unitOrder', () => {
+  const input = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'варвар', health: 10 },
+  ];
+  const received = unitOrder(input);
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+    { name: 'варвар', health: 10 },
+  ];
+  expect(received).toBe(expected);
+});
