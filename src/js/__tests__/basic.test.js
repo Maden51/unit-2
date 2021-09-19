9ff2a92c6f0c86d859911da349c04bedd71c8787
Maden5126 <madenon123@gmail.com>
2021-09-19 14:24:07 +0300

    toBe check

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -14,21 +14,10 @@ test('unitOrder', () => {
     { name: 'мечник', health: 10 },
     { name: 'варвар', health: 10 },
   ];
+  try {
+    expect(received).toBe(expected);
+  } catch (e) {
+    console.log('doesnt work');
+  }
   expect(received).toEqual(expected);
-});
-test('unitOrder', () => {
-  const input = [
-    { name: 'мечник', health: 10 },
-    { name: 'маг', health: 100 },
-    { name: 'лучник', health: 80 },
-    { name: 'варвар', health: 10 },
-  ];
-  const received = unitOrder(input);
-  const expected = [
-    { name: 'маг', health: 100 },
-    { name: 'лучник', health: 80 },
-    { name: 'мечник', health: 10 },
-    { name: 'варвар', health: 10 },
-  ];
-  expect(received).toBe(expected);
 });
